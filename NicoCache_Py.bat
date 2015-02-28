18af6665f551421cf6e3e58052f5e8ac0f978365
LHO425 <lho425@gmail.com>
2015-02-28 16:33:28 +0900

    modify bat file for compatibility
    .batのコピペ部分をうまく抽象化した

--- NicoCache_Py.bat
+++ NicoCache_Py.bat
@@ -1,7 +1,14 @@
 @echo off
-
-
 cd /d %~dp0
 shift 
-"C:\Python27\python.exe" nicocache.py  %*  2>&1| nkf -u -W -s
+
+if "%0"=="-nkf32" (
+    SET nkf=nkf32
+) else (
+    SET nkf=nkf
+)
+
+
+
+"C:\Python27\python.exe" nicocache.py  %*  2>&1| %nkf% -u -W -s
 pause
--- NicoCache_Py_32.bat
+++ NicoCache_Py_32.bat
@@ -3,5 +3,6 @@
 
 cd /d %~dp0
 shift 
-"C:\Python27\python.exe" nicocache.py  %*  2>&1| nkf32 -u -W -s
-pause
+
+NicoCache_Py.bat -nkf32  %*
+
