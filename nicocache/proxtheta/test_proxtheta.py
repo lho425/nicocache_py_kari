5445a7d072c0a3941d9193d9edb2a853ed4a1d29
LHO425 <lho425@gmail.com>
2016-01-22 07:29:04 +0900

    Make a bit of refactoring

--- nicocache/proxtheta/test_proxtheta.py
+++ nicocache/proxtheta/test_proxtheta.py
@@ -5,17 +5,15 @@ import unittest
 import os
 import StringIO
 import shutil
-from proxtheta import httpmes, utility
-import proxtheta
-import proxtheta.utility
-import proxtheta.utility.client
+from .core import httpmes
+from .utility.client import make_nonproxy_camouflaged_request
 verbosity=1
 
 class Test_nonproxy_camouflaged_request(unittest.TestCase):
     def test(self):
         req = httpmes.HTTPRequest(("GET", ("http", "host", None, "/", "", ""), "HTTP/1.1"))
                 
-        req = proxtheta.utility.client.make_nonproxy_camouflaged_request(req)
+        req = make_nonproxy_camouflaged_request(req)
         
         self.assertIsNotNone(req)
         
