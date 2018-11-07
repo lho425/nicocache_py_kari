c89dad1dc849771b48028d686eb330a431d437b0
LHO425 <lho425@gmail.com>
2018-11-07 21:45:22 +0900

    Add common name for mitm ssl cert

--- gen-ssl-mitm-keys.sh
+++ gen-ssl-mitm-keys.sh
@@ -3,5 +3,5 @@
 mkdir -p mitm
 
 openssl genrsa 2048 > mitm/server.key
-openssl req -subj '/C=JP' -new -key mitm/server.key > mitm/server.csr
+openssl req -subj '/C=JP/CN=*.nicovideo.jp' -new -key mitm/server.key > mitm/server.csr
 openssl x509 -days 36500 -req -signkey mitm/server.key < mitm/server.csr > mitm/server.crt -extfile mitm/san.ext  # -extfile is needed for google chrome 
