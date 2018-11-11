37033edd73f6b19feeee222cd707568b060a566b
LHO425 <lho425@gmail.com>
2018-11-11 22:15:13 +0900

    Fix

--- NicoCache_Py.sh
+++ NicoCache_Py.sh
@@ -2,7 +2,7 @@
 
 cd $(dirname $0)
 
-export PYENV_ROOT="$PWD/.pyenv"
+export PYENV_ROOT="$PWD/pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
 if command -v pyenv 1>/dev/null 2>&1; then
   eval "$(pyenv init -)"
