e4659d5efff296c55a3ff5fd461172f24e19d292
LHO425 <lho425@gmail.com>
2018-11-11 22:08:03 +0900

    Fix prepare_pyenv.sh

--- prepare_pyenv.sh
+++ prepare_pyenv.sh
@@ -2,7 +2,7 @@
 
 git clone https://github.com/pyenv/pyenv.git
 
-export PYENV_ROOT="$PWD/pyenv"
+export PYENV_ROOT="$PWD/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
 if command -v pyenv 1>/dev/null 2>&1; then
   eval "$(pyenv init -)"
