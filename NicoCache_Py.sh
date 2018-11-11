b3d1665e6bbbb89370362392341f8c907d28456e
LHO425 <lho425@gmail.com>
2018-11-11 22:09:40 +0900

    Update NicoCache_Py.sh for pyenv

--- NicoCache_Py.sh
+++ NicoCache_Py.sh
@@ -2,4 +2,10 @@
 
 cd $(dirname $0)
 
+export PYENV_ROOT="$PWD/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+if command -v pyenv 1>/dev/null 2>&1; then
+  eval "$(pyenv init -)"
+fi
+
 python NicoCache_Py.py "$@"
