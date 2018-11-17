519e80551c324dc96d69e3e2a00428d6ae4d8290
LHO425 <lho425@gmail.com>
2018-11-17 17:30:10 +0900

    Add save api stub

--- nicocache/apiserver.py
+++ nicocache/apiserver.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 
 def create_flask_app(video_cache_manager):
     app = Flask(__name__)
+
+    @app.route("/nicocache/api/v1/save/<video_id>", methods=["PUT"])
+    def save(video_id):
+        return jsonify({}), 500
 
     return app
--- nicocache/test_apiserver.py
+++ nicocache/test_apiserver.py
@@ -2,6 +2,10 @@ from unittest import TestCase
 
 from . import apiserver
 
+from logging import getLogger
+
+logger = getLogger(__name__)
+
 
 def _create_test_client(video_cache_manager):
     app = apiserver.create_flask_app(video_cache_manager)
@@ -13,3 +17,10 @@ class TestMyFlaskApp(TestCase):
         client = _create_test_client(None)
         res = client.get("/")
         self.assertEqual(404, res.status_code)
+
+    def test_save(self):
+        client = _create_test_client(None)
+        res = client.put("/nicocache/api/v1/save/sm1")
+        if res.status_code != 200:
+            logger.error("save api failed, response: %s", res)
+        self.assertEqual(200, res.status_code)
